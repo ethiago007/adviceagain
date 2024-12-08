3080efc35f3f5ed695c6585902414d8c44982432
Mohammed Kamaldeen <mohammedkamaldeen204@gmail.com>
2024-12-08 23:41:55 +0100

    done and dusted

---
+++ Advice-generator/src/App.jsx
@@ -0,0 +1,16 @@
+import './css/App.css'
+import './css/card.css'
+import BasicGrid22 from './component/card';
+
+
+
+function App() {
+    return (
+        <div className="App" >
+
+           <BasicGrid22 />
+        </div>
+    );
+}
+
+export default App;
